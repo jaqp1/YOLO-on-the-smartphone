7b8cf222a46cd353ef293ca3ec4d8c61f6df3015
jaqp1 <kuba.wenek@wp.pl>
2026-03-19 08:08:50 +0100

    first successfull build

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,38 @@
-import { ThemedText } from "@/components/themed-text";
 import "@/global.css";
-import { Redirect, useRouter } from "expo-router";
-import { Platform, StatusBar } from "react-native";
+import { Redirect } from "expo-router";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   Camera,
   useCameraDevice,
+  useCameraFormat,
   useCameraPermission,
 } from "react-native-vision-camera";
 
 export default function HomeScreen() {
   const { hasPermission } = useCameraPermission();
   const microphonePermission = Camera.getMicrophonePermissionStatus();
-  const router = useRouter();
-  const redirectToPermissions =
-    !hasPermission || microphonePermission === "not-determined";
-
   const device = useCameraDevice("back");
 
-  if (redirectToPermissions) return <Redirect href={"/Permissions"} />;
+  // Wybieramy optymalny format (rozdzielczość i FPS)
+  const format = useCameraFormat(device, [
+    { videoResolution: { width: 1280, height: 720 } },
+    { fps: 30 },
+  ]);
+
+  if (!hasPermission || microphonePermission === "not-determined")
+    return <Redirect href={"/Permissions"} />;
   if (!device) return <></>;
 
   return (
-    <>
-      <SafeAreaView
-        className={`flex-1 ${Platform.OS === "android" ? StatusBar.currentHeight : 0}`}
-      >
-        <Camera className="flex-1" device={device} isActive />
-        <ThemedText></ThemedText>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Camera
+        style={StyleSheet.absoluteFill} // Użyj stylu zamiast NativeWind dla testu
+        device={device}
+        format={format} // Dodaj to
+        pixelFormat="yuv" // 'yuv' jest najbardziej kompatybilny na Androidzie
+        isActive={true}
+      />
+    </SafeAreaView>
   );
 }
